f4022ff66c798bdd1562be457c0a4749d6f67eaf
Jerome72 <jerome72@gmail.com>
2026-08-27 15:54:30 +0200

    Restore original autoplay behavior in clip color editor

--- web/extender.js
+++ web/extender.js
@@ -1384,8 +1384,7 @@ async function openClipColorEditor(node, runtime, clipIndex) {
     video.addEventListener("loadedmetadata", () => {
         video.currentTime = loopStart;
         updateLiveFilter();
-        
-        // video.play().catch(() => {}); // Disabled to prevent autoplay
+        video.play().catch(() => {});
     });
     video.addEventListener("timeupdate", () => {
         if (video.currentTime >= loopEnd - 0.015 || video.currentTime < loopStart - 0.05) {
